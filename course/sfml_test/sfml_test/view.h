327d48d5a55de57057084767becfbe1ca575f9c0
andrew <andreykichatoff@gmail.com>
2018-04-14 23:03:19 +0300

    Добавил класс врагов, пробую взаимодействие, изменил управление, сделал двери/ключи однотипными

--- course/sfml_test/sfml_test/view.h
+++ course/sfml_test/sfml_test/view.h
@@ -4,6 +4,10 @@ using namespace sf;
 
 View view; //объ€вили sfml объект "вид", который и €вл€етс€ камерой
 //слежение камеры за персом
+
 void getplayercoordinates(float x, float y) {
 	view.setCenter(x+15, y+15);
+	if (Keyboard::isKeyPressed(Keyboard::Space))
+		view.setSize(2 * map_width * 32, 2 * map_height * 32);
+	else view.setSize(map_width * 32, map_height * 32);
 }
